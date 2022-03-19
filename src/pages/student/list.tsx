818d16381da7737c3eddcaaf2eaf82da53b7a7fa
Maick Souza <maick_a_s@msn.com>
2022-03-19 14:21:34 -0300

    styles(web): header #2

--- src/pages/student/list.tsx
+++ src/pages/student/list.tsx
@@ -1,30 +1,9 @@
+import { Header } from "../../components/Header";
+
 export default function StudentList() {
   return (
     <>
-      <header className='flex items-center justify-between'>
-        <div className=' w-full flex'>
-          <h1 className='px-4 text-red-500 font-bold'>GYMPOINT</h1>
-          <div className='border-l-2 border-l-red-500 flex items-center '>
-            <a className='text-sm px-4 font-semibold' href=''>
-              ALUNOS
-            </a>
-            <a className='text-sm px-4' href=''>
-              PLANOS
-            </a>
-            <a className='text-sm px-4' href=''>
-              MATRÍCULAS
-            </a>
-            <a className='text-sm px-4' href=''>
-              PEDIDIDOS DE AUXÍLIO
-            </a>
-          </div>
-        </div>
-
-        <div className='flex flex-col '>
-          <h4>Maick Souza</h4>
-          <p className='text-red-500'>Sair do sistema</p>
-        </div>
-      </header>
+      <Header />
     </>
   );
 }
