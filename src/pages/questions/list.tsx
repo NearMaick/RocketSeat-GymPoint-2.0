cb321d11e1b94502b308896ffa30887e62bca78e
Maick Souza <maick_a_s@msn.com>
2022-03-22 09:25:13 -0300

    styles(web): questionsList #2

--- src/pages/questions/list.tsx
+++ src/pages/questions/list.tsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+import Modal from "react-modal";
 import { Header } from "../../components/Header";
+
+Modal.setAppElement("#__next");
 
 const questions = [
   { id: "1", student: "Maick Souza" },
@@ -8,8 +12,25 @@ const questions = [
 ];
 
 export default function QuestionsList() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [answer, setAnswer] = useState("");
+
+  function openModal() {
+    setModalIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    console.log("modal aberto");
+  }
+
+  function closeModal() {
+    console.log("modal fechado");
+    setModalIsOpen(false);
+    console.log(answer);
+  }
+
   return (
-    <div className='h-screen w-screen bg-gray-100'>
+    <div className=' h-screen w-screen bg-gray-100'>
       <Header />
       <section className='flex items-center justify-between p-4'>
         <div>
@@ -24,13 +45,38 @@ export default function QuestionsList() {
           <div key={question.id} className='grid grid-cols-2 border-b-2'>
             <p className='py-4'>{question.student}</p>
             <div className='w-full py-4 flex justify-end'>
-              <button type='button' onClick={() => {}}>
+              <button type='button' onClick={openModal}>
                 <a className='px-2 text-blue-500'>responder</a>
               </button>
             </div>
           </div>
         ))}
       </section>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={{
+          overlay: {
+            backgroundColor: "#666",
+            opacity: 0.9,
+          },
+        }}
+        className='bg-white h-1/2 w-1/2 mx-auto my-56 flex flex-col items-center justify-center'
+        contentLabel='Example Modal'>
+        <h2>Pergunta do aluno</h2>
+        <p>
+          Olá pessoal da academia, gostaria de saber se quando acordar devo
+          ingerir batata doce e frango logo de primeira, preparar as marmitas e
+          lotar a geladeira? Dou um pico de insulina e jogo o hipercalórico?
+        </p>
+        <h2>Sua resposta</h2>
+        <textarea
+          onChange={(event) => setAnswer(event.target.value)}
+          placeholder='Digite sua resposta aqui'
+        />
+        <button onClick={closeModal}>Responder Aluno</button>
+      </Modal>
     </div>
   );
 }
