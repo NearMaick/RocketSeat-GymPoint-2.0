b7c942a75f55c452b5fd1b1aab98c4f805e4e583
Maick Souza <maick_a_s@msn.com>
2022-04-02 15:43:15 -0300

    feat(web): Header Links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,46 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 export function Header() {
+  const { pathname } = useRouter();
+
   return (
     <header className='bg-white border-2 rounded-md flex items-center justify-between'>
       <div className=' w-full flex'>
         <h1 className='px-4 text-red-500 font-bold'>GYMPOINT</h1>
         <div className='border-l-2 border-l-red-500 flex items-center '>
-          <a className='text-sm px-4 font-semibold' href=''>
-            ALUNOS
-          </a>
-          <a className='text-sm px-4' href=''>
-            PLANOS
-          </a>
-          <a className='text-sm px-4' href=''>
-            MATRÍCULAS
-          </a>
-          <a className='text-sm px-4' href=''>
-            PEDIDIDOS DE AUXÍLIO
-          </a>
+          <Link href='/student/list'>
+            <a
+              className={`text-sm px-4 ${
+                pathname === "/student/list" && "font-semibold"
+              }`}>
+              ALUNOS
+            </a>
+          </Link>
+          <Link href='/options/list'>
+            <a
+              className={`text-sm px-4 ${
+                pathname === "/options/list" && "font-semibold"
+              }`}>
+              PLANOS
+            </a>
+          </Link>
+          <Link href='/registrations/list'>
+            <a
+              className={`text-sm px-4 ${
+                pathname === "/registrations/list" && "font-semibold"
+              }`}>
+              MATRÍCULAS
+            </a>
+          </Link>
+          <Link href='/questions/list'>
+            <a
+              className={`text-sm px-4 ${
+                pathname === "/questions/list" && "font-semibold"
+              }`}>
+              PEDIDIDOS DE AUXÍLIO
+            </a>
+          </Link>
         </div>
       </div>
 
